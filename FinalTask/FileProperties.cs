5ad8fa233c8aeed33bb82d06c90cf880680280f9
Lukasz Rogiewicz <lrogiewicz@sdl.com>
2019-07-21 16:27:25 +0200

    v1.1

---
+++ FinalTask/FileProperties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    class FileProperties
+    {
+        public string JobID { get; set; }
+        public string SourceLang { get; set; }
+        public string TargetLang { get; set; }
+        public string Filename { get; set; }
+
+        public FileProperties(string jobID, string sourceLang, string targetLang, string filename)
+        {
+            this.JobID = jobID;
+            this.SourceLang = sourceLang;
+            this.TargetLang = targetLang;
+            this.Filename = filename;
+
+        }
+
+    }
+}
--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Xml.Linq;
 using System.Configuration;
 using System.Text.RegularExpressions;
+using System.IO.Compression;
 
 namespace FinalTask
 {
@@ -16,32 +17,50 @@ namespace FinalTask
         {
 
             //FileInfo
-            string properFilesPath = ConfigurationManager.AppSettings["inputPath"];
-            string[] files = Directory.GetFiles(properFilesPath, "*.sdlxliff", SearchOption.AllDirectories);
+
+            string correctedFilesPath = ConfigurationManager.AppSettings["correctedFiles"];
+            string[] correctedFiles = Directory.GetFiles(correctedFilesPath, "*.sdlxliff", SearchOption.AllDirectories);
+            string wrongFilesPath = ConfigurationManager.AppSettings["wrongFiles"];
+            string[] wrongFiles = Directory.GetFiles(wrongFilesPath, "*.zip", SearchOption.AllDirectories);
             XDocument xml;
-            List<String> fileAttribute = new List<string>();
             string sourceLang;
             string targetLang;
             string targetFolderName;
             string fileOriginalPath;
             string jobID;
-            string patternJobID= "\\[0-9]+\\";
+            string patternJobID = "\\\\[0-9]+\\\\";
             Regex rgxJobID = new Regex(patternJobID);
+            string filename;
 
-            foreach (var file in files)
+
+            foreach (var correctfile in correctedFiles)
             {
-                xml = XDocument.Load(file);
+                xml = XDocument.Load(correctfile);
 
                 fileOriginalPath = ((System.Xml.Linq.XElement)xml.Root.LastNode).FirstAttribute.Value;
                 sourceLang = ((System.Xml.Linq.XElement)xml.Root.LastNode).LastAttribute.PreviousAttribute.NextAttribute.PreviousAttribute.Value.ToUpper();
                 targetLang = ((System.Xml.Linq.XElement)xml.Root.LastNode).LastAttribute.Value;
                 targetFolderName = targetLang.Substring(targetLang.Length - 2);
                 MatchCollection matches = rgxJobID.Matches(fileOriginalPath);
-                jobID = matches[0].ToString();
+                jobID = matches[0].ToString().Replace("\\","");
+                filename = fileOriginalPath.Substring(fileOriginalPath.LastIndexOf("\\")+1);
 
+
+                FileProperties fileProperties = new FileProperties(jobID,sourceLang,targetLang, filename);
+                
                 Console.WriteLine(jobID);
                 Console.WriteLine(sourceLang);
                 Console.WriteLine(targetFolderName);
+                Console.WriteLine(filename);
+
+                foreach (string wrongFile in wrongFiles)
+                {
+                    if (wrongFile.StartsWith(jobID))
+                    {
+                        //https://stackoverflow.com/questions/12553809/how-to-check-whether-file-exists-in-zip-file-using-dotnetzip
+                    }
+                }
+
             }
             
             Console.ReadLine();
